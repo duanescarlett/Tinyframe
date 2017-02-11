becae894e00087d1444bb344d6ff8eef0d30e65d
Dos <duanescarlett@gmail.com>
2017-02-11 02:57:23 -0500

    Did some html/css work on the welcome page Signed-off-by: Dos <duanescarlett@gmail.com>

--- View/welcome.php
+++ View/welcome.php
@@ -17,11 +17,44 @@
 	</div>
 </div>
 	<div class="contain">
-		<h4>
+		<h1>Welcome</h1>
+		<p>
 		TinyFrame is a PHP web application framework that is simple and very customizable. TinyFrame belives that it should 
 		allow the developer to have the ability to customize the entire framework, this will allow the developer to create a 
 		truly unique web application quickly. TinyFrame also features an admin backend that is also easy to customize, out the 
 		box it robust authentication system and allows you to track the application's users.
-		</h4>
+		</p>
+		
+		
 	</div>
+	
+	
+		<div class="container blocks">
+			<div>
+				<h4>
+					Quick and easy setup
+				</h4>
+				
+				<p>
+					<p>1. Go to the config.php file inside of the Core folder and assign the project name to the $baseUrl variable.</p>
+					<p>2. Enter your database info.</p>
+					<p>3. Open the Terminal.bat file and run this command "php composer.phar update"</p>
+					<p>4. Start creating amazing php web applications.</p>
+				</p>
+			</div>
+		</div>
+	
+		<div class="container blocks">
+			<div>
+				<h4>
+					Checkout the pre-built Admin section
+				</h4>
+				
+				<p>
+					<p>1. Register as the sites Admin <\The-name-of-your-site\> /admin/register</p>
+					<p>2. Access the admin section at <\The-name-of-your-site\> /admin</p>
+				</p>
+			</div>
+		</div>
+	
 </body>
